94be8ff9a6e157cb6b703f82412299b09c9041a8
Bene Lorant <lbene@pitechplus.com>
2015-01-28 14:44:16 +0200

    Created activities block.

---
+++ sites/all/themes/yamato/templates/node/node--activity--teaser.tpl.php
@@ -0,0 +1,16 @@
+<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 ">
+  <a href="<?php print $node_url?>">
+    <div class="box-yamato">
+      <ul class="box box1">
+        <li class="box-icon">
+          <?php print render($content['field_logo']); ?>
+        </li>
+        <li class="box-header">
+          <h4><?php print $title; ?></h4>
+        </li>
+        <li class="box-text"><?php print render($content['field_summary']); ?></li>
+      </ul>
+    </div>
+  </a>
+  <p></p>
+</div>
